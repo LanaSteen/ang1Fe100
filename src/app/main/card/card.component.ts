bec41fab2ed2b08b821e3ff9ee45fec0cf361332
Karatel13 <davitgogoladze2000@gmail.com>
2025-03-27 17:52:29 +0400

    V 1.0 added object to main(cmp) and passed object via Input to Card(cmp)

--- src/app/main/card/card.component.ts
+++ src/app/main/card/card.component.ts
@@ -11,7 +11,5 @@ import { ButtonComponent } from '../button/button.component';
   standalone: true
 })
 export class CardComponent {
-  @Input() title: string = 'Dota 2';
-  @Input() description: string = 'Free To Play';
-  @Input() image?: string = "https://upload.wikimedia.org/wikipedia/ru/8/8e/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_Dota_2.jpg";
+ @Input() product!: {id: number; title: string; description: string; image: string;}
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -9,5 +9,10 @@ import { CardComponent } from "./card/card.component";
   standalone: true
 })
 export class MainComponent {
-
+  product={
+    id: 1,
+    title: "Dota 2",
+    description: "Free To Play",    
+    image: "https://upload.wikimedia.org/wikipedia/ru/8/8e/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_Dota_2.jpg"
+    };
 }
