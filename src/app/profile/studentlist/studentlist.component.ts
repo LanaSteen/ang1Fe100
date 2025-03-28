03ff097919b79c042e006b731ea9e4ffe4f5594f
Karatel13 <davitgogoladze2000@gmail.com>
2025-03-28 18:19:01 +0400

    V1.11 fixed standalone studentList cmp

--- src/app/profile/studentlist/studentlist.component.ts
+++ src/app/profile/studentlist/studentlist.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-studentlist',
+  standalone: true,
   imports: [],
   templateUrl: './studentlist.component.html',
   styleUrl: './studentlist.component.scss'
