ea7ed2199affc35bc74c18a29de36b0f9546c914
Karatel13 <davitgogoladze2000@gmail.com>
2025-04-01 13:38:54 +0400

    V1.231 added error link comp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ErrorComponent } from './error/error.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: "full"},
   { path: 'home', component: MainComponent },
-  { path: 'profile', component: ProfileComponent }
+  { path: 'profile', component: ProfileComponent },
+
+
+
+
+  {path: '**', component: ErrorComponent}
+
 ];
