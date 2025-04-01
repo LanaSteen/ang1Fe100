64a8f3a3b37e6590486b36cbc270d34ad5388588
Karatel13 <davitgogoladze2000@gmail.com>
2025-04-01 13:26:16 +0400

    V1.2303 Fixed epmty route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { MainComponent } from './main/main.component';
 import { ProfileComponent } from './profile/profile.component';
 
 export const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: "full"},
   { path: 'home', component: MainComponent },
   { path: 'profile', component: ProfileComponent }
 ];
