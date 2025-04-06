615c5346a04e3e18ef7cb7d4fb541278fe3b38d3
Karatel13 <davitgogoladze2000@gmail.com>
2025-04-06 17:12:06 +0400

    V1.242 add Details component for seemore button

--- src/app/users/user-card/user-card.component.ts
+++ src/app/users/user-card/user-card.component.ts
@@ -1,14 +1,24 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { IUser } from '../../Models/user';
+
 
 @Component({
   selector: 'app-user-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [RouterModule],
   templateUrl: './user-card.component.html',
   styleUrls: ['./user-card.component.scss']
 })
 export class UserCardComponent {
   @Input() userList!: IUser;
+  
+
+  constructor(private router: Router) {}
+  
+
+  goToDetails() {
+    this.router.navigate(['/details', this.userList.id]);
+  }
 }
