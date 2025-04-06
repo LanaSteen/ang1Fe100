e3c0e4c7333d98fb298540533a52af3e7df8b30d
Karatel13 <davitgogoladze2000@gmail.com>
2025-04-06 12:30:26 +0400

    V1.24 Added user obj and service and userType

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ErrorComponent } from './error/error.component';
+// import { DetailsComponent } from './details/details.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: "full"},
   { path: 'home', component: MainComponent },
   { path: 'profile', component: ProfileComponent },
+  // { path: 'details/:id', component:DetailsComponent},
 
 
 
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { SignUpComponent } from "./sign-up/sign-up.component";
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [SignUpComponent, FormsModule],
+  imports: [SignUpComponent, FormsModule,RouterModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
